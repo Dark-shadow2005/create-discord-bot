bcc4dcad6dee06c23e98d8f9bc1bd9f5673b4707
Dark-shadow2005 <53373820+Dark-shadow2005@users.noreply.github.com>
2019-08-27 03:25:19 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
+  token: ('NTg2Njc1Mzg5MzU5OTgwNTU0.XWTZQw.lywm8TvvYCsQVmahNOJ9mmlHZrk' in process.env ? process.env : require('../config')).TOKEN,
   prefix: '.',
   commandDelimiter: '\n',
   commandLimit: '5'
